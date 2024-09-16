8584af50a5645eb84495cd457ebf3bcbf1fe1592
Sb99CODE <138096045+Sb99CODE@users.noreply.github.com>
2024-09-16 21:42:34 +0530

    message

--- src/pages/Admin/AdminHome.js
+++ src/pages/Admin/AdminHome.js
@@ -9,36 +9,21 @@ const AdminHome = () => {
       <div className="container">
         <div className="d-felx flex-column mt-4">
           <h1>
-            Welcome Admin <i className="text-success">{user?.name}</i>
+            Welcome Admin <i className="text-success"> {user?.name}</i>
           </h1>
           <h3>Manage BloodConnect App </h3>
           <hr />
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
-            explicabo animi blanditiis incidunt dicta quia, quibusdam facere
-            corporis! Dolores, reprehenderit cum sed repellat laudantium
-            architecto natus est nostrum accusamus, odio aspernatur minima
-            fugiat quam molestiae nisi. Temporibus impedit dolorem quia.
-            Distinctio modi non excepturi illo odio voluptatum quae nostrum a
-            temporibus sequi! Explicabo, quasi consequatur ad qui quos labore
-            distinctio voluptates alias nostrum ab dicta aspernatur molestias
-            adipisci quibusdam error ipsa. Totam, tenetur dolores eaque tempora
-            officiis deserunt assumenda? Rerum nemo est nihil laudantium
-            necessitatibus. Possimus, voluptatem voluptates blanditiis quas
-            aspernatur, quam, quaerat minus maiores ipsam sint perferendis
-            dolor. Dignissimos voluptatem doloribus sint in quis omnis, atque
-            neque praesentium voluptatum suscipit. Quas esse, accusantium maxime
-            obcaecati iure officiis aperiam minus alias quod cum quos qui
-            voluptatibus, numquam, ad id dolore odit! Minima laudantium sunt,
-            explicabo nesciunt quos voluptatibus qui libero eligendi praesentium
-            debitis obcaecati similique assumenda nobis labore totam dolore
-            perferendis adipisci fugiat quibusdam tempore doloremque voluptatum
-            accusantium. Accusamus, incidunt sequi. Esse sunt officia fuga,
-            officiis saepe tempora repellat suscipit aliquid cupiditate
-            perferendis, asperiores architecto molestiae rem iste eaque
-            molestias reiciendis. Laborum modi asperiores, reprehenderit
-            assumenda numquam, vitae exercitationem illum nesciunt, dolorum
-            deleniti accusamus consequatur id. Necessitatibus dolore ad fugit?
+            BloodConnect is a comprehensive blood bank management website built
+            using the MERN (MongoDB, Express, React, Node.js) stack. It is
+            designed to streamline the management of blood inventories, donor
+            information, and blood donation events. The application leverages
+            Redux for efficient state management and ensures scalable data flow
+            across components. Additionally, JWT (JSON Web Tokens) is
+            implemented for secure user authentication, providing role-based
+            access for donors, organizations, and administrators. BloodConnect
+            aims to enhance the efficiency and reliability of blood banks
+            through a user-friendly, secure, and scalable platform.
           </p>
         </div>
       </div>
